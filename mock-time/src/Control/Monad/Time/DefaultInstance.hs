ded6bf2d49a084b8688099c768892ec2748ca28f
Kamil Dworakowski <kamil@dworakowski.name>
2020-05-14 22:13:43 +0200

    More Overlappable

--- mock-time/src/Control/Monad/Time/DefaultInstance.hs
+++ mock-time/src/Control/Monad/Time/DefaultInstance.hs
@@ -10,6 +10,7 @@ import Control.Monad.Trans (liftIO, MonadIO)
 import Data.Time (getCurrentTime)
 import Control.Concurrent (threadDelay)
 
-instance (Monad m, MonadIO m) => MonadTime m where
+instance {-# OVERLAPPABLE #-}
+  (Monad m, MonadIO m) => MonadTime m where
   getCurrentTime = liftIO Data.Time.getCurrentTime
   threadDelay delay = liftIO $ Control.Concurrent.threadDelay delay
