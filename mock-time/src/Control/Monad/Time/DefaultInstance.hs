9795baa628d7963cc154d017d2f2f3fdadf14a73
Kamil Dworakowski <kamil@dworakowski.name>
2020-11-20 15:57:46 +0100

    Clarify the docs for mock time

--- mock-time/src/Control/Monad/Time/DefaultInstance.hs
+++ mock-time/src/Control/Monad/Time/DefaultInstance.hs
@@ -1,8 +1,11 @@
 {-# LANGUAGE UndecidableInstances #-}
 {-# LANGUAGE FlexibleInstances #-}
--- | The default instance for 'MonadTime', using only the underlying system time.
+-- | The instance of 'MonadTime' for IO, using the underlying system time and does
 -- This is to be used in production code, or in thoses tests where controling time
 -- is not needed.
+--
+-- This is only and instance for IO, for instances for monad transformers see
+-- 'Control.Monad.Trans.SimulatedTime'.
 module Control.Monad.Time.DefaultInstance (
 module Control.Monad.Time.DefaultInstance,
 module Control.Monad.Time) where
